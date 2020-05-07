e35f167bc7d1c0d5bff591f359a006830011a2f9
SAUBHAGYA SAHOO <saubhagyasahoo2@gmail.com>
2020-05-07 12:08:25 +0530

    week 1

---
+++ DAY6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& a) {
+        int size=a.size();
+        int maj_index = 0, count = 1;
+    for (int i = 1; i < size; i++)
+    {
+        if (a[maj_index] == a[i])
+            count++;
+        else
+            count--;
+        if (count == 0)
+        {
+            maj_index = i;
+            count = 1;
+        }
+    }
+    return a[maj_index];
+    }
+};
